93c35578fe248c5f497bb67628376829f881d723
tasos.coding <tasos.coding@outlook.com>
2016-12-13 18:03:18 +0200

    initial commit

---
+++ 232test1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _232test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(0, 0);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(1041, 347);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(886, 353);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(155, 45);
+            this.sendBtn.TabIndex = 1;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(12, 353);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(868, 22);
+            this.data.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 634);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.TextBox data;
+    }
+}
+
---
+++ 232test1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace _232test1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.Two);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            output.Text += port.ReadExisting() + "\n";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            output.Text += "Incoming Data:\n";
+
+            // Attach a method to be called when there
+            // is data waiting in the port's buffer
+            port.DataReceived += new
+              SerialDataReceivedEventHandler(port_DataReceived);
+
+            // Begin communications
+            port.Open();
+
+
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            port.Write((0x06000714).ToString());
+            // port.Write(Convert.ToInt16(data.Text).ToString());
+        }
+    }
+
+}
+
+//  https://www.codeproject.com/articles/20929/simple-modbus-protocol-in-c-net
