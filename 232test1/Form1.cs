a7bf66b27e89c632994be1375cf3b88b68d21e72
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2016-12-15 03:15:24 +0200

    Use modbus for communicating via serial port

--- 232test1/Form1.cs
+++ 232test1/Form1.cs
@@ -1,51 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Modbus_Poll_CS;
 using System.IO.Ports;
 
 namespace _232test1
 {
     public partial class Form1 : Form
     {
-        private SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.Two);
+        private modbus link;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            // Show all the incoming data in the port's buffer
-            output.Text += port.ReadExisting() + "\n";
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            output.Text += "Incoming Data:\n";
-
-            // Attach a method to be called when there
-            // is data waiting in the port's buffer
-            port.DataReceived += new
-              SerialDataReceivedEventHandler(port_DataReceived);
-
-            // Begin communications
-            port.Open();
-
-
+            link.Open("COM3", 9600, 8, Parity.None, StopBits.Two);
         }
 
         private void sendBtn_Click(object sender, EventArgs e)
         {
-            port.Write((0x06000714).ToString());
-            // port.Write(Convert.ToInt16(data.Text).ToString());
+            string[] values = data.Text.Split(' ');
+            UInt16 addr = Convert.ToUInt16(values[0]);
+            bool ret;
+            if (values.Length > 1)
+            {
+                short[] val = {Convert.ToInt16(values[1])};
+                ret = link.SendFc16(1, addr, 1, val);
+                if (ret) output.Text += "Successfully wrote ";
+                else output.Text += "Failed to write ";
+                output.Text += values[1] + " to address " + values[0] + "\n";
+            }
+            else
+            {
+                short[] val = new short[1];
+                ret = link.SendFc3(1, addr, 1, ref val);
+                if (ret) output.Text += "Read from " + values[0] + ": " + val[0].ToString() + "\n";
+                else output.Text += "Read from " + values[0] + " failed\n";
+            }
+            if (!ret) output.Text += "Status: " + link.modbusStatus + "\n";
         }
+
     }
 
 }
