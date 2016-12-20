c457ae1b486e2cea6996ed27996e1c5c6d119b97
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2016-12-20 02:23:47 +0200

    Fix window title

--- 232test1/Form1.Designer.cs
+++ 232test1/Form1.Designer.cs
@@ -68,7 +68,7 @@
             this.Controls.Add(this.sendBtn);
             this.Controls.Add(this.output);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Inverter control interface";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
