92eafbe70691739b9c92c0595bb2150ede94477d
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2016-12-20 02:20:54 +0200

    Enable sending commands by pressing return

--- 232test1/Form1.Designer.cs
+++ 232test1/Form1.Designer.cs
@@ -60,6 +60,7 @@
             // 
             // Form1
             // 
+            this.AcceptButton = this.sendBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1053, 634);
