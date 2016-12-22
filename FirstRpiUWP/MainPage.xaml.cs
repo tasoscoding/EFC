5d447fd2e97866bd0632ba71f67be5279d800e77
tasos.coding <tasos.coding@outlook.com>
2016-12-22 17:21:53 +0200

    Attempt to make WUP work

--- FirstRpiUWP/MainPage.xaml.cs
+++ FirstRpiUWP/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Modbus_Poll_CS;
+using Windows.Devices.SerialCommunication;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,16 +12,21 @@ namespace FirstRpiUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private modbus link = new modbus();
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        LoadCompletedEventHandler
-
         private void button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            link.Open("COM3", 9600, 8, SerialParity.None, SerialStopBitCount.Two);
         }
     }
 }
